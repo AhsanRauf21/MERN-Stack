ff667734694edc43a9dd1a09c5853c481734f188
AhsanRauf21 <forbidden020@gmail.com>
2025-09-02 21:34:31 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors({
-  origin: "https://mern-stack-beige-psi.vercel.app",
+  origin: "https://mern-stack-beige-psi.vercel.app/login",
   credentials: true
 }));
 app.use(express.json({}))
