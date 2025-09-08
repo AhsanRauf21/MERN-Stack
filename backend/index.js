5dc59908ec5f1fa6e65ecf9b8f48bb2b3d85501a
AhsanRauf21 <forbidden020@gmail.com>
2025-09-08 19:46:08 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,7 @@ require("dotenv").config({
 })
 require('colors')
 const app = require("./src/app");
-const cors = require('cors')
 
-app.use(cors({
-  origin: "https://mern-stack-beige-psi.vercel.app",
-  credentials: true
-}));
 const { ConnectDB } = require("./src/db.config");
 const port  = process.env.PORT || 4500
 ConnectDB()
