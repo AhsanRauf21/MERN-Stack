5d253f5bf33b94cddf3852ddcd86047a187c813b
AhsanRauf21 <forbidden020@gmail.com>
2025-11-10 00:16:26 +0500

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-indigo-500 py-10 text-white mt-10">
+    <footer className="bg-indigo-500 py-10 text-white fixed mb-0">
       {/* Bottom bar */}
       <div className="bg-indigo-600  text-center text-sm text-indigo-200">
         <p>
