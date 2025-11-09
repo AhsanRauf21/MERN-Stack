8bde642053f752075fc17ea35918c6fdb0c9db72
AhsanRauf21 <forbidden020@gmail.com>
2025-11-10 00:02:02 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ const app = require("./src/app");
 const { ConnectDB } = require("./src/db.config");
 const port  = process.env.PORT || 4500
 ConnectDB()
+app.get('/',(req,res) => {
+    res.send(`<h1>Server Running<h1/>`)
+})
+
 app.listen(port,()=>{
     console.log(`App is listening at http://localhost:${port}`.bgBlue);
     
