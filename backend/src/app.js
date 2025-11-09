ed1e017272ba0c073a50fabd03e8cfb8cd12539b
AhsanRauf21 <forbidden020@gmail.com>
2025-11-10 00:20:39 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,7 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 
-app.use(cors({
-  origin: process.env.Frontend_URL,
-  credentials: true
-}));
+app.use(cors('*'));
 app.use(express.json({}))
 app.use(express.urlencoded({extended:false}))
 
